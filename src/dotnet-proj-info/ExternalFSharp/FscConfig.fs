90b2fb6ea41bf842d40e90da8a55eb7e8b65faec
Enrico Sada <enrico@sada.io>
2017-10-19 19:41:20 +0200

    srtp

--- src/dotnet-proj-info/ExternalFSharp/FscConfig.fs
+++ src/dotnet-proj-info/ExternalFSharp/FscConfig.fs
@@ -2,10 +2,16 @@ module Dotnet.ProjInfo.FakeMsbuildTasks
 
 open System
 
-let config props =
+// SRTP: how to set a property:
+//
+// let inline setReferences (e: ^a) x =
+//     (^a: (member set_References: Microsoft.Build.Framework.ITaskItem array -> unit) (e, x))
+//
 
-    let fsc = Microsoft.FSharp.Build.Fsc()
-    
+type ITaskItemArray = Microsoft.Build.Framework.ITaskItem array
+
+let inline config' props (fsc: ^a) =
+
     let m : Map<string, string> = props |> Map.ofList
     let prop k =
         match m |> Map.tryFind k with
@@ -40,55 +46,58 @@ let config props =
             |> Array.map (fun x -> x :> Microsoft.Build.Framework.ITaskItem)
             |> f
 
-    bind (fun prop -> fsc.BaseAddress <- prop) "BaseAddress"
-    bind (fun prop -> fsc.CodePage <- prop) "CodePage"
-    bindBool (fun prop -> fsc.DebugSymbols <- prop) "DebugSymbols"
-    bind (fun prop -> fsc.DebugType <- prop) "DebugType"
-    bindArray (fun prop -> fsc.DefineConstants <- prop) "DefineConstants"
-    bindBool (fun prop -> fsc.DelaySign <- prop) "DelaySign"
-    bind (fun prop -> fsc.DisabledWarnings <- prop) "DisabledWarnings"
-    bind (fun prop -> fsc.DocumentationFile <- prop) "DocumentationFile"
-    bind (fun prop -> fsc.DotnetFscCompilerPath <- prop) "DotnetFscCompilerPath"
-    bindBool (fun prop -> fsc.EmbedAllSources <- prop) "EmbedAllSources"
-    bind (fun prop -> fsc.Embed <- prop) "Embed"
-    bind (fun prop -> fsc.GenerateInterfaceFile <- prop) "GenerateInterfaceFile"
-    bindBool (fun prop -> fsc.HighEntropyVA <- prop) "HighEntropyVA"
-    bind (fun prop -> fsc.KeyFile <- prop) "KeyFile"
-    bind (fun prop -> fsc.LCID <- prop) "LCID"
-    //bindBool (fun prop -> fsc.NoFramework <- prop) "true"
-    bindBool (fun prop -> fsc.Optimize <- prop) "Optimize"
-    bind (fun prop -> fsc.OtherFlags <- prop) "OtherFlags"
-    bind (fun prop -> fsc.OutputAssembly <- prop) "OutputAssembly"
-    bind (fun prop -> fsc.PdbFile <- prop) "PdbFile"
-    bind (fun prop -> fsc.Platform <- prop) "Platform"
-    bindBool (fun prop -> fsc.Prefer32Bit <- prop) "Prefer32Bit"
-    bind (fun prop -> fsc.PreferredUILang <- prop) "PreferredUILang"
-    bindBool (fun prop -> fsc.ProvideCommandLineArgs <- prop) "ProvideCommandLineArgs"
-    bindBool (fun prop -> fsc.PublicSign <- prop) "PublicSign"
-    bindArray (fun prop -> fsc.References <- prop) "References"
-    bind (fun prop -> fsc.ReferencePath <- prop) "ReferencePath"
-    bindArray (fun prop -> fsc.Resources <- prop) "Resources"
-    bindBool (fun prop -> fsc.SkipCompilerExecution <- prop) "SkipCompilerExecution"
-    bind (fun prop -> fsc.SourceLink <- prop) "SourceLink"
-    bindArray (fun prop -> fsc.Sources <- prop) "Sources"
-    bindBool (fun prop -> fsc.Tailcalls <- prop) "Tailcalls"
-    bind (fun prop -> fsc.TargetType <- prop) "TargetType"
-    bind (fun prop -> fsc.TargetProfile <- prop) "TargetProfile"
-    bind (fun prop -> fsc.ToolExe <- prop) "ToolExe"
-    bind (fun prop -> fsc.ToolPath <- prop) "ToolPath"
-    bindBool (fun prop -> fsc.TreatWarningsAsErrors <- prop) "TreatWarningsAsErrors"
-    bindBool (fun prop -> fsc.UseStandardResourceNames <- prop) "UseStandardResourceNames"
-    bindBool (fun prop -> fsc.Utf8Output <- prop) "Utf8Output"
-    bind (fun prop -> fsc.VersionFile <- prop) "VersionFile"
-    bindBool (fun prop -> fsc.VisualStudioStyleErrors <- prop) "VisualStudioStyleErrors"
-    bind (fun prop -> fsc.WarningLevel <- prop) "WarningLevel"
-    bind (fun prop -> fsc.WarningsAsErrors <- prop) "WarningsAsErrors"
-    bind (fun prop -> fsc.Win32ManifestFile <- prop) "Win32ManifestFile"
-    bind (fun prop -> fsc.Win32ResourceFile <- prop) "Win32ResourceFile"
-    bind (fun prop -> fsc.SubsystemVersion <- prop) "SubsystemVersion"
+    "BaseAddress"|> bind (fun prop -> (^a: (member set_BaseAddress: string -> unit) (fsc, prop)))
+    "CodePage" |> bind (fun prop -> (^a: (member set_CodePage: string -> unit) (fsc, prop)))
+    "DebugSymbols" |> bindBool (fun prop -> (^a: (member set_DebugSymbols: bool -> unit) (fsc, prop)))
+    "DebugType" |> bind (fun prop -> (^a: (member set_DebugType: string -> unit) (fsc, prop)))
+    "DefineConstants" |> bindArray (fun prop -> (^a: (member set_DefineConstants: ITaskItemArray -> unit) (fsc, prop)))
+    "DelaySign" |> bindBool (fun prop -> (^a: (member set_DelaySign: bool -> unit) (fsc, prop)))
+    "DisabledWarnings" |> bind (fun prop -> (^a: (member set_DisabledWarnings: string -> unit) (fsc, prop)))
+    "DocumentationFile" |> bind (fun prop -> (^a: (member set_DocumentationFile: string -> unit) (fsc, prop)))
+    "DotnetFscCompilerPath" |> bind (fun prop -> (^a: (member set_DotnetFscCompilerPath: string -> unit) (fsc, prop)))
+    "EmbedAllSources" |> bindBool (fun prop -> (^a: (member set_EmbedAllSources: bool -> unit) (fsc, prop)))
+    "Embed" |> bind (fun prop -> (^a: (member set_Embed: string -> unit) (fsc, prop)))
+    "GenerateInterfaceFile" |> bind (fun prop -> (^a: (member set_GenerateInterfaceFile: string -> unit) (fsc, prop)))
+    "HighEntropyVA" |> bindBool (fun prop -> (^a: (member set_HighEntropyVA: bool -> unit) (fsc, prop)))
+    "KeyFile" |> bind (fun prop -> (^a: (member set_KeyFile: string -> unit) (fsc, prop)))
+    "LCID" |> bind (fun prop -> (^a: (member set_LCID: string -> unit) (fsc, prop)))
+    "NoFramework" |> bindBool (fun prop -> (^a: (member set_NoFramework: bool -> unit) (fsc, prop)))
+    "Optimize" |> bindBool (fun prop -> (^a: (member set_Optimize: bool -> unit) (fsc, prop)))
+    "OtherFlags" |> bind (fun prop -> (^a: (member set_OtherFlags: string -> unit) (fsc, prop)))
+    "OutputAssembly" |> bind (fun prop -> (^a: (member set_OutputAssembly: string -> unit) (fsc, prop)))
+    "PdbFile" |> bind (fun prop -> (^a: (member set_PdbFile: string -> unit) (fsc, prop)))
+    "Platform" |> bind (fun prop -> (^a: (member set_Platform: string -> unit) (fsc, prop)))
+    "Prefer32Bit" |>bindBool (fun prop -> (^a: (member set_Prefer32Bit: bool -> unit) (fsc, prop)))
+    "PreferredUILang" |> bind (fun prop -> (^a: (member set_PreferredUILang: string -> unit) (fsc, prop)))
+    "ProvideCommandLineArgs" |> bindBool (fun prop -> (^a: (member set_ProvideCommandLineArgs: bool -> unit) (fsc, prop)))
+    "PublicSign" |> bindBool (fun prop -> (^a: (member set_PublicSign: bool -> unit) (fsc, prop)))
+    "References" |> bindArray (fun prop -> (^a: (member set_References: ITaskItemArray -> unit) (fsc, prop)))
+    "ReferencePath" |> bind (fun prop -> (^a: (member set_ReferencePath: string -> unit) (fsc, prop)))
+    "Resources" |> bindArray (fun prop -> (^a: (member set_Resources: ITaskItemArray -> unit) (fsc, prop)))
+    "SkipCompilerExecution" |> bindBool (fun prop -> (^a: (member set_SkipCompilerExecution: bool -> unit) (fsc, prop)))
+    "SourceLink" |> bind (fun prop -> (^a: (member set_SourceLink: string -> unit) (fsc, prop)))
+    "Sources" |> bindArray (fun prop -> (^a: (member set_Sources: ITaskItemArray -> unit) (fsc, prop)))
+    "Tailcalls" |> bindBool (fun prop -> (^a: (member set_Tailcalls: bool -> unit) (fsc, prop)))
+    "TargetType" |> bind (fun prop -> (^a: (member set_TargetType: string -> unit) (fsc, prop)))
+    "TargetProfile" |> bind (fun prop -> (^a: (member set_TargetProfile: string -> unit) (fsc, prop)))
+    "ToolExe" |> bind (fun prop -> (^a: (member set_ToolExe: string -> unit) (fsc, prop)))
+    "ToolPath" |> bind (fun prop -> (^a: (member set_ToolPath: string -> unit) (fsc, prop)))
+    "TreatWarningsAsErrors" |> bindBool (fun prop -> (^a: (member set_TreatWarningsAsErrors: bool -> unit) (fsc, prop)))
+    "UseStandardResourceNames" |> bindBool (fun prop -> (^a: (member set_UseStandardResourceNames: bool -> unit) (fsc, prop)))
+    "Utf8Output" |> bindBool (fun prop -> (^a: (member set_Utf8Output: bool -> unit) (fsc, prop)))
+    "VersionFile" |> bind (fun prop -> (^a: (member set_VersionFile: string -> unit) (fsc, prop)))
+    "VisualStudioStyleErrors" |> bindBool (fun prop -> (^a: (member set_VisualStudioStyleErrors: bool -> unit) (fsc, prop)))
+    "WarningLevel" |> bind (fun prop -> (^a: (member set_WarningLevel: string -> unit) (fsc, prop)))
+    "WarningsAsErrors" |> bind (fun prop -> (^a: (member set_WarningsAsErrors: string -> unit) (fsc, prop)))
+    "Win32ManifestFile" |> bind (fun prop -> (^a: (member set_Win32ManifestFile: string -> unit) (fsc, prop)))
+    "Win32ResourceFile" |> bind (fun prop -> (^a: (member set_Win32ResourceFile: string -> unit) (fsc, prop)))
+    "SubsystemVersion" |> bind (fun prop -> (^a: (member set_SubsystemVersion: string -> unit) (fsc, prop)))
 
-    let responseFileText = fsc.InternalGenerateResponseFileCommands()
+    let responseFileText = (^a: (member GenerateResponseFileCommands: unit -> string) (fsc))
 
     responseFileText.Split([| Environment.NewLine |], StringSplitOptions.RemoveEmptyEntries)
     |> List.ofArray
 
+let config props =
+    let fsc = Microsoft.FSharp.Build.Fsc()
+    config' props fsc
