daa347e55183f1a1163f4271e5bacb050a752b11
Enrico Sada <enrico@sada.io>
2019-01-08 23:59:12 +0100

    add runprocess from fsac

--- src/Dotnet.ProjInfo.Workspace/Common.fs
+++ src/Dotnet.ProjInfo.Workspace/Common.fs
@@ -7,3 +7,33 @@ module Option =
     match option with
     | None -> defaultValue
     | Some x -> x
+
+module Utils =
+
+  let runProcess (log: string -> unit) (workingDir: string) (exePath: string) (args: string) =
+      let psi = System.Diagnostics.ProcessStartInfo()
+      psi.FileName <- exePath
+      psi.WorkingDirectory <- workingDir
+      psi.RedirectStandardOutput <- true
+      psi.RedirectStandardError <- true
+      psi.Arguments <- args
+      psi.CreateNoWindow <- true
+      psi.UseShellExecute <- false
+
+      use p = new System.Diagnostics.Process()
+      p.StartInfo <- psi
+
+      p.OutputDataReceived.Add(fun ea -> log (ea.Data))
+
+      p.ErrorDataReceived.Add(fun ea -> log (ea.Data))
+
+      // printfn "running: %s %s" psi.FileName psi.Arguments
+
+      p.Start() |> ignore
+      p.BeginOutputReadLine()
+      p.BeginErrorReadLine()
+      p.WaitForExit()
+
+      let exitCode = p.ExitCode
+
+      exitCode, (workingDir, exePath, args)
