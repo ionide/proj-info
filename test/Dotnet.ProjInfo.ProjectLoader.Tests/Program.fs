1f6e8dbaf268fedc4c2ed5a2fc5d7ae602304327
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2020-12-06 19:14:18 +0100

    Add unit tests for the WorkspaceLoader

--- test/Dotnet.ProjInfo.ProjectLoader.Tests/Program.fs
+++ test/Dotnet.ProjInfo.ProjectLoader.Tests/Program.fs
@@ -3,16 +3,10 @@
 open System
 open Dotnet.ProjInfo
 
+open Expecto
+
+
 [<EntryPoint>]
 let main argv =
     let toolsPath = Init.init ()
-    printfn "TOOLS: %A" toolsPath
-
-    //let path = System.IO.Path.GetFullPath @"D:\Programowanie\Projekty\Ionide\FsAutoComplete\src\FsAutoComplete\FsAutoComplete.fsproj"
-    let path = System.IO.Path.GetFullPath  "../../src/Dotnet.ProjInfo.Workspace.FCS/Dotnet.ProjInfo.Workspace.FCS.fsproj"
-    // let path = System.IO.Path.GetFullPath  "Dotnet.ProjInfo.ProjectLoader.Tests.fsproj"
-
-    let loader = WorkspaceLoader.Create(toolsPath, fun p _ -> p)
-    let result = loader.LoadProject path
-    printfn "%A" result
-    0 // return an integer exit code
+    Tests.runTests defaultConfig (Tests.tests toolsPath)
