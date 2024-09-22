4ddc65a436ac309a7fd08da8dd212d5e6d068dca
Chet Husk <chusk3@gmail.com>
2024-09-22 16:34:41 -0500

    convert tests and build project to NuGet CPM

--- test/Ionide.ProjInfo.Tests/Program.fs
+++ test/Ionide.ProjInfo.Tests/Program.fs
@@ -1,28 +1,23 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
 open Ionide.ProjInfo
-open Ionide.ProjInfo.ProjectSystem
-
 open Expecto
 open Expecto.Impl
 open Expecto.Logging
+open System.Threading
 
 let toolsPath = Init.init (IO.DirectoryInfo Environment.CurrentDirectory) None
 
 [<Tests>]
 let tests = Tests.tests toolsPath
 
-
 [<EntryPoint>]
 let main argv =
-    let toolsPath = Init.init (IO.DirectoryInfo Environment.CurrentDirectory) None
+    let _ = Init.init (IO.DirectoryInfo Environment.CurrentDirectory) None
+    use cts = new CancellationTokenSource()
+    Console.CancelKeyPress.Add(fun _ -> cts.Cancel())
+    AppDomain.CurrentDomain.ProcessExit.Add(fun _ -> cts.Cancel())
 
-    Tests.runTestsWithArgs
-        {
-            defaultConfig with
-                printer = TestPrinters.summaryPrinter defaultConfig.printer
-                verbosity = LogLevel.Verbose
-        }
-        argv
-        tests
+    runTestsWithCLIArgsAndCancel cts.Token [
+            Printer (TestPrinters.summaryPrinter defaultConfig.printer)
+            Verbosity Verbose
+        ] argv tests
